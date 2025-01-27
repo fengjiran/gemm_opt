e0cf3314280d30c2dbd1b5174447d44d4183d535
fengjiran <fengjiran@foxmail.com>
2025-01-27 18:24:42 +0800

    gen random matrix

--- matmul.h
+++ matmul.h
@@ -13,12 +13,12 @@ static double get_time(timespec* start, timespec* end) {
 
 template<typename T = float,
          typename = std::enable_if_t<std::is_floating_point_v<T>>>
-std::vector<T> GenRandomMatrix(size_t m, size_t n, T low = 0, T high = 1) {
+std::vector<T> GenRandomMatrix(size_t m, size_t stride, T low = 0, T high = 1) {
     std::random_device rd;
     std::mt19937 gen(rd());
-    std::vector<T> matrix(m * n);
+    std::vector<T> matrix(m * stride);
     std::uniform_real_distribution<T> dist(low, high);
-    for (size_t i = 0; i < m * n; ++i) {
+    for (size_t i = 0; i < m * stride; ++i) {
         matrix[i] = dist(gen);
     }
     return matrix;
@@ -27,12 +27,12 @@ std::vector<T> GenRandomMatrix(size_t m, size_t n, T low = 0, T high = 1) {
 template<typename T,
          typename = void,
          typename = std::enable_if_t<std::is_integral_v<T>>>
-std::vector<T> GenRandomMatrix(size_t m, size_t n, T low = 0, T high = 10) {
+std::vector<T> GenRandomMatrix(size_t m, size_t stride, T low = 0, T high = 10) {
     std::random_device rd;
     std::mt19937 gen(rd());
-    std::vector<T> matrix(m * n);
+    std::vector<T> matrix(m * stride);
     std::uniform_int_distribution<T> dist(low, high);
-    for (size_t i = 0; i < m * n; ++i) {
+    for (size_t i = 0; i < m * stride; ++i) {
         matrix[i] = dist(gen);
     }
     return matrix;
